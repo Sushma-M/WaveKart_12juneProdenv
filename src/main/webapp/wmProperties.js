5ef7e50926eceaca6c06f726594b69c5e2bd7332
root <root@58f198d91948>
2016-06-12 06:04:35 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "WaveKart",
+  "homePage" : "Main",
+  "name" : "WaveKart",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.03"
+};
